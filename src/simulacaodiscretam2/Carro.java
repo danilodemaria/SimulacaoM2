103cac24d10a7c3974716ecde724cb7413d30b85
lorexp <vinixp.vp@gmail.com>
2019-05-26 16:11:39 -0300

    Iniciei a parada

---
+++ src/simulacaodiscretam2/Carro.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulacaodiscretam2;
+
+/**
+ *
+ * @author vinit
+ */
+public class Carro {
+    
+    private int TEC;
+    private int TS;
+    private int entrar_fila;
+    private int entrar_lavacao;
+    private int tempo_fila;
+    private int tempo_lavacao;
+
+    /**
+     * @return the TEC
+     */
+    public int getTEC() {
+        return TEC;
+    }
+
+    /**
+     * @param TEC the TEC to set
+     */
+    public void setTEC(int TEC) {
+        this.TEC = TEC;
+    }
+
+    /**
+     * @return the TS
+     */
+    public int getTS() {
+        return TS;
+    }
+
+    /**
+     * @param TS the TS to set
+     */
+    public void setTS(int TS) {
+        this.TS = TS;
+    }
+
+    /**
+     * @return the entrar_fila
+     */
+    public int getEntrar_fila() {
+        return entrar_fila;
+    }
+
+    /**
+     * @param entrar_fila the entrar_fila to set
+     */
+    public void setEntrar_fila(int entrar_fila) {
+        this.entrar_fila = entrar_fila;
+    }
+
+    /**
+     * @return the entrar_lavacao
+     */
+    public int getEntrar_lavacao() {
+        return entrar_lavacao;
+    }
+
+    /**
+     * @param entrar_lavacao the entrar_lavacao to set
+     */
+    public void setEntrar_lavacao(int entrar_lavacao) {
+        this.entrar_lavacao = entrar_lavacao;
+    }
+
+    /**
+     * @return the tempo_fila
+     */
+    public int getTempo_fila() {
+        return tempo_fila;
+    }
+
+    /**
+     * @param tempo_fila the tempo_fila to set
+     */
+    public void setTempo_fila(int tempo_fila) {
+        this.tempo_fila = tempo_fila;
+    }
+
+    /**
+     * @return the tempo_lavacao
+     */
+    public int getTempo_lavacao() {
+        return tempo_lavacao;
+    }
+
+    /**
+     * @param tempo_lavacao the tempo_lavacao to set
+     */
+    public void setTempo_lavacao(int tempo_lavacao) {
+        this.tempo_lavacao = tempo_lavacao;
+    }
+}
